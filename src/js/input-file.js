a383edd8fd4be425acc296a7c43c1e590b440332
Ivan <plastun.vanya@yandex.ru>
2018-12-04 00:05:26 +0300

    Added two sections for upload file and added mixin for input file

--- src/js/input-file.js
+++ src/js/input-file.js
@@ -10,9 +10,108 @@
 
 		input.addEventListener( 'change', function( e )
 		{
-			var fileName = '';
-			if( this.files && this.files.length > 1 )
-				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			var fileName = '',
+				desctiprionFiles = '',
+				numberOfFiles = this.files.length,
+				remain = numberOfFiles % 10;
+
+			if( this.files && numberOfFiles > 1 ) { 
+				if(numberOfFiles < 10) {
+					switch(remain) {
+						case 2:
+							desctiprionFiles = ' файла выбрано';
+						break;
+						case 3:
+							desctiprionFiles = ' файла выбрано';
+						break;
+						case 4:
+							desctiprionFiles = ' файла выбрано';
+						break;
+						case 5:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 6:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 7:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 8:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 9:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+					}
+				} else if(numberOfFiles >= 10 && numberOfFiles < 20) {
+					switch(remain) {
+						case 0 :
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 1:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 2:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 3:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 4:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 5:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 6:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 7:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 8:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 9:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+					}
+				} else {
+					switch(remain) {
+						case 0:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 1:
+							desctiprionFiles = ' файл выбрано';
+						break;
+						case 2:
+							desctiprionFiles = ' файла выбрано';
+						break;
+						case 3:
+							desctiprionFiles = ' файла выбрано';
+						break;
+						case 4:
+							desctiprionFiles = ' файла выбрано';
+						break;
+						case 5:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 6:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 7:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 8:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+						case 9:
+							desctiprionFiles = ' файлов выбрано';
+						break;
+					}
+				}
+				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', numberOfFiles + desctiprionFiles);
+			}
 			else
 				fileName = e.target.value.split( '\\' ).pop();
 
